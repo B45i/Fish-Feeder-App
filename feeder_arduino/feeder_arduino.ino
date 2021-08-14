293afd5569d2efb22d322a1dd7db9487fa170a3d
B45i <amalshajan2011@gmail.com>
2021-08-14 13:45:21 +0530

    serve html using SPIFS

--- feeder_arduino/feeder_arduino.ino
+++ feeder_arduino/feeder_arduino.ino
@@ -1,18 +1,44 @@
-#include <LittleFS.h>
-#include "configure.h"
-#include "fileSystem.h"
-#include "ArduinoJson.h"
-#include "jsonParse.h"
-#include "rtcFile.h"
-#include "controller.h"
+#include <ESP8266WiFi.h>
+//#include <LittleFS.h>
+#include <ESPAsyncWebServer.h> //  https://github.com/me-no-dev/ESPAsyncWebServer and https://github.com/me-no-dev/ESPAsyncTCP
+#include <WiFiClient.h>
+
+//#include "configure.h"
+//#include "fileSystem.h"
+//#include "ArduinoJson.h"
+//#include "jsonParse.h"
+//#include "rtcFile.h"
+//#include "controller.h"
+
+AsyncWebServer server(80);
 
 void setup()
 {
-  Serial.begin(9600);
-  init_rtc();
-  initPins();
-  readDataFromFile();
-  getData();
+  Serial.begin(115200);
+  //  LittleFS.begin();
+  //  init_rtc();
+  //  initPins();
+  //  readDataFromFile();
+  //  getData();
+
+  // todo: set as access point
+  WiFi.begin("ZTE_2.4G_ExQCMa", "NullReferenceException#123");
+  WiFi.mode(WIFI_STA);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  SPIFFS.begin();
+  server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
+  server.begin();
 }
 
 void loop()
@@ -23,7 +49,7 @@ void loop()
 }
 
 /*
-If the submit button is pressed,
+  If the submit button is pressed,
   store the json to the String variable 'data'
   call the 'writeDataToFile()' to store the data to the flash.
   call 'setTime()'
