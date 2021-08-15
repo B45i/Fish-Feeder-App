4002cefdfa0f1bcea452c5b645be7c92b58ed334
B45i <amalshajan2011@gmail.com>
2021-08-15 10:55:28 +0530

    dve and prod urls

--- src/app/services/feeder.service.ts
+++ src/app/services/feeder.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IFeedSettings } from '../models/dto';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FeederService {
-  readonly bseURL = 'http://localhost:3000';
+  readonly bseURL = environment.bseURL;
 
   constructor(private http: HttpClient) {}
 
